e0ea9267f405e447de8e2657a2113a456fa29ba9
ayushnemmaniwar <68007760+ayushnemmaniwar@users.noreply.github.com>
2025-01-11 23:05:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function firstWord(s) {
   // your code here
-}
+	let n = s.length;
+    let i=0;
+    while(i<n && s[i]===' ') {
+        i++;
+    }
+    let ans='';
+    while(i<n && s[i]!==' ') {
+        ans=ans+s[i];
+        i++;
+    }
+    return ans;
+} 
 
 // Do not change the code below
 
